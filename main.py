b7642513daedc07e978600dd7902d808d31a7067
Vlad Makarenko <vladislav.topdevs@gmail.com>
2021-12-28 18:28:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+from interpreter.interpreter import evaluate
+import os.path
+import threading
+
+
+def run_thread(code, number):
+    threading.Thread(target=evaluate, args=(code, number)).start()
+
+
+programs = {}
+
+
+def main():
+    choice = input('''
+1) Список программ
+2) Создать программу (построчный ввод)
+3) Cоздать программу (из текстового файла)
+4) Запустить поток 
+    ''')
+    if choice == '1':
+        for k in programs.keys():
+            print(k)
+
+    elif choice == '2':
+        name = input('Введите название программы: ')
+        print('Введите код программы:')
+        code = ''
+        while True:
+            line = input()
+            if line == 'KONETS':
+                break
+            code += line
+            code += '\n'
+
+        programs[name] = code
+
+    elif choice == '3':
+        name = input('Введите название программы: ')
+        file_path = input('Введите путь к файлу с кодом: ')
+        if not os.path.exists(file_path):
+            print('Файл не найден')
+
+        else:
+            code = ''.join(open(file_path).readlines())
+            programs[name] = code
+
+    elif choice == '4':
+        name = input('Введите название программы для запуска: ')
+        if name not in programs:
+            print('Такой програмы нет')
+
+        else:
+            number = int(input('Введите количество итераций: '))
+            run_thread(programs[name], number)
+
+
+if __name__ == '__main__':
+    while True:
+        main()
